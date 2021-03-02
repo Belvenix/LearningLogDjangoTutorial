11e98b79cd223c80a10ae85f4f39f2b19da7784e
jbelter <kuba.belter@gmail.com>
2021-03-02 15:18:32 +0100

    Add new functionality - add new topic to list of topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 from learning_logs.models import Topic
-
+from .forms import TopicForm
 
 def index(request):
     """Strona główna dla aplikacji Learning Log."""
@@ -23,3 +24,18 @@ def topic(request, topic_id):
     entries = topic_entity.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Dodaj nowy temat."""
+    if request.method != 'POST':
+        # Nie przekazano żadnych danych, należy utworzyć pusty formularz
+        form = TopicForm()
+    else:
+        # Przekazano dane za pomocą żądania POST, należy je przetworzyć
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
